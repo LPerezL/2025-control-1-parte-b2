d9d96c5522107641e9a8279a8b7cd712c16ef6d4
Islu64 <lp1795456@gmail.com>
2025-11-06 18:37:08 +0100

    tests hechos

--- proyecto-maven/src/test/java/es/upm/grise/profundizacion/cruisecontroller/CruiseControlTest.java
+++ proyecto-maven/src/test/java/es/upm/grise/profundizacion/cruisecontroller/CruiseControlTest.java
@@ -1,5 +1,30 @@
 package es.upm.grise.profundizacion.cruisecontroller;
+    import static org.junit.jupiter.api.Assertions.assertThrows;
+    import static org.junit.jupiter.api.Assertions.assertTrue;
+    import org.junit.jupiter.api.Test;
 
+import es.upm.grise.profundizacion.cruisecontrol.CruiseControl;
+import es.upm.grise.profundizacion.cruisecontrol.IncorrectSpeedSetException;
+import es.upm.grise.profundizacion.cruisecontrol.SpeedSetAboveSpeedLimitException;
 class CruiseControlTest {
+
+    @Test
+    void constructorTest(){
+        CruiseControl cruCon = new CruiseControl();
+        assertTrue(cruCon.getSpeedSet() == null&&cruCon.getSpeedLimit() == null);
+    }
+
+    @Test
+    void setSpeedSetNegativeTest()  throws SpeedSetAboveSpeedLimitException, IncorrectSpeedSetException {
+        CruiseControl cruCon = new CruiseControl();
+        assertThrows(IncorrectSpeedSetException.class, () -> cruCon.setSpeedSet(-15));
+    }
+
+    @Test
+    void setSpeedSetAboveLimitTest()  throws SpeedSetAboveSpeedLimitException, IncorrectSpeedSetException {
+        CruiseControl cruCon = new CruiseControl();
+        cruCon.setSpeedLimit(100);
+        assertThrows(SpeedSetAboveSpeedLimitException.class, () -> cruCon.setSpeedSet(101));
+    }
 	
 }
